c71e1400189a69d6f212aad0c397edd6076d3e3c
antidale <antidale@gmail.com>
2024-01-24 23:08:34 -0800

    Adding trackers for restream

---
+++ FE.Restream/Enums/IconType.cs
@@ -0,0 +1,24 @@
+﻿namespace FE.Restream.Enums
+{
+    public enum IconType
+    {
+        Crystal,
+        Pass,
+        Hook,
+        DarknessCrystal,
+        EarthCrystal,
+        TwinHarp,
+        Package,
+        SandRuby,
+        BaronKey,
+        MagmaKey,
+        TowerKey,
+        LucaKey,
+        Adamant,
+        LegendSword,
+        Pan,
+        Spoon,
+        RatTail,
+        PinkTail
+    }
+}
---
+++ FE.Restream/Models/Icon.cs
@@ -0,0 +1,26 @@
+﻿using FE.Restream.Enums;
+
+namespace FE.Restream.Models
+{
+    public class Icon(IconType type, IconState state = IconState.Gray)
+    {
+        public string Class { get => type.ToString(); }
+        public string Name { get; private set; } = $"{type}-{state}.png";
+
+        public void HandleClick()
+        {
+            state = state switch
+            {
+                IconState.Gray => IconState.Color,
+                IconState.Color => IconState.Check,
+                IconState.Check => IconState.Gray,
+                _ => IconState.Gray
+            };
+
+            Name = UpdateName();
+        }
+
+        private string UpdateName() => $"{type}-{state}.png";
+
+    }
+}
