66e84038994482db7721458275e7b44af0269fb4
antidale <antidale@tellah.life>
2024-07-16 21:34:31 -0700

    Layout first pass incoporating FeTracker.Common.RazorComponents

---
+++ FE.Restream/Models/Objective.cs
@@ -0,0 +1,9 @@
+﻿namespace FE.Restream.Models
+{
+    public class Objective
+    {
+        public bool PlayerOne { get; set; }
+        public bool PlayerTwo { get; set; }
+        public string Text { get; set; } = string.Empty;
+    }
+}
